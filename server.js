bd6a98a80a9130ea6e8449b242949fad21691130
MagangaCode123 <tomdanty@gmail.com>
2025-01-26 16:31:42 +0300

    fix:server config

---
+++ server.js
@@ -0,0 +1,59 @@
+require("dotenv").config();
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const Order = require("./models/order"); // Import the Order model
+const validate = require("./middleware/decodeJWT");
+const authorization = require("./routes/authentication");
+
+// Initialize the app
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/auth", validate, authorization);
+
+// Order routes
+
+// POST route to create a new order
+app.post("/api/orders", async (req, res) => {
+  try {
+    const { customerName, items, totalPrice } = req.body;
+    const newOrder = new Order({ customerName, items, totalPrice });
+
+    await newOrder.save();
+    res.status(201).json(newOrder);
+  } catch (err) {
+    res.status(400).json({ error: "Failed to create order" });
+  }
+});
+
+// GET route to retrieve all orders
+app.get("/api/orders", async (req, res) => {
+  try {
+    const orders = await Order.find();
+    res.status(200).json(orders);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to retrieve orders" });
+  }
+});
+
+// Start the server
+mongoose
+  .connect(process.env.DATABASE_URI)
+  .then(() => {
+    app.listen(process.env.PORT, (req, res) => {
+      console.log(req);
+      console.log("Connected to db & listening on port 4000");
+    });
+  })
+  .catch((err) => {
+    console.error("error:", err);
+  });

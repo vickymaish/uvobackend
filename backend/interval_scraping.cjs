86a45fd064bd65cfaf2e77d6d332447da9f7612e
victor maina <57532773+vickymaish@users.noreply.github.com>
2025-02-05 07:11:36 +0300

    Update interval_scraping.cjs

--- backend/interval_scraping.cjs
+++ backend/interval_scraping.cjs
@@ -420,8 +420,8 @@ let isScraping = false; // Declare isScraping globally
 (async () => {
     // Launch Puppeteer browser
     const browser = await puppeteer.launch({
-        headless: false,
-        executablePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe',
+        headless: true,
+       // executablePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe',
         timeout: 400000,
         slowMo: 10,
         args: [

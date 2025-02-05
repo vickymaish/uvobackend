4643ddbe43735bdd63991aa3e1804d0f1092e88b
victor maina <57532773+vickymaish@users.noreply.github.com>
2025-02-05 08:34:34 +0300

    Update interval_scraping.cjs

--- backend/interval_scraping.cjs
+++ backend/interval_scraping.cjs
@@ -421,8 +421,9 @@ let isScraping = false; // Declare isScraping globally
     // Launch Puppeteer browser
     const browser = await puppeteer.launch({
         headless: true,
-         executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || "/usr/bin/google-chrome",
-        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || "/opt/render/.cache/puppeteer/chrome/linux-131.0.6778.204/chrome-linux64/chrome",
+        executablePath: "/opt/render/.cache/puppeteer/chrome/linux-131.0.6778.204/chrome-linux64/chrome",
+        //executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || "/usr/bin/google-chrome",
+        //executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || "/opt/render/.cache/puppeteer/chrome/linux-131.0.6778.204/chrome-linux64/chrome",
         //executablePath: "/opt/render/.cache/puppeteer/chrome/linux-131.0.6778.204/chrome-linux64/chrome",
         timeout: 400000,
         slowMo: 10,
